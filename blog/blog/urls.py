3bd34886fed1163ebf80aa2330a92344f3e7c74d
chai200703 <abcdjoshi33@gmail.com>
2024-05-31 15:24:32 +0530

    updated techblog

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from techblog import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home),
+    path('',views.index),
     path('signup/',views.user_signup, name="signup"),
     path('login/',views.user_login, name="login"),
     path('logout/',views.user_logout, name="logout"),
